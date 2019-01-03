7d358e55cdeb144857b7625432342162ac7de0ae
zhangle <778437470@qq.com>
2019-01-03 09:09:10 +0800

    字符串常量池

--- src/test/java/com/example/demo/other/Test5.java
+++ src/test/java/com/example/demo/other/Test5.java
@@ -3,27 +3,33 @@ package com.example.demo.other;
 import com.example.demo.annotation.Test;
 
 /**
+ * 字符串判断
  * @author byron
  * @date 2018/12/25 11:34
  */
 @Test("haha")
 public class Test5 {
 
+//  private static final String AAA = "hello";
   public static void main(String[] args) {
 //    Test test = Test5.class.getAnnotation(Test.class);
 //    System.out.println(test.value());
 
-    new Thread(new Runnable() {
-      @Override
-      public void run() {
-        try {
-          Thread.sleep(5*1000);
-        }catch (InterruptedException e){
-          e.printStackTrace();
-        }
-        System.out.println("hahaha");
-      }
-    }).start();
-    System.out.println("fsgsadg");
+//    String a1 = new String("hello");
+//    String a3 = a1.intern();
+//    System.out.println(a==a1);
+//    System.out.println(a==a3);
+
+
+    //String a = "hello";
+    String str = new StringBuilder("hel").append("lo").toString();
+  //  String str = new String("hello'");
+    String str2 = new StringBuilder("ja").append("va").toString();
+    //在常量池中没有"hello"这个常量，所以在new的时候会创建两个。一个存放在常量池，另外一个存放在堆中。所以这两个对象是同一个地址。所以结果为true
+    System.out.println(str.intern() == str);
+    //由于"java"在class加载的时候就已经存在了（具体存在位置在Version这个类里）。所以new的时候只会创建一个。存放在堆中，
+    // 所以这两个地址是不同的两个地址
+    System.out.println(str2.intern() == str2);
+
   }
 }
---
+++ src/test/java/com/example/demo/other/Test6.java
@@ -0,0 +1,32 @@
+package com.example.demo.other;
+
+/**
+ * @author byron
+ * @date 2018/12/28 17:52
+ */
+public class Test6 extends test7{
+  static {
+    System.out.println("dfsfsdfs");
+  }
+}
+class AAA{
+  public static void main(String[] args) {
+//    //String string = new StringBuilder("AA").append("AA").toString();
+//    String string = new String("AAAA");
+//    String string1 = string.intern();
+//    String string2 = "AAAA";
+//
+////    System.out.println(string2 == string);
+////    System.out.println(string1 == string);
+//    System.out.println(string1 == string2);
+//    System.out.printf("%1.2f",11.2102);
+    System.out.println(Test6.string);
+  }
+}
+
+class test7{
+  public static String string = "ddd";
+  static {
+    System.out.println("12312分公司答复");
+  }
+}
