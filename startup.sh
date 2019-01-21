bafbc2593f77e9099d89adb6ae224406a0415422
zhangle <778437470@qq.com>
2019-01-21 11:40:52 +0800

    修改为sh脚本4

--- startup.sh
+++ startup.sh
@@ -5,8 +5,8 @@ if [ ${pid} ]
 then
    echo :App  is  running pid=${pid}
    kill -9 ${pid}
+   echo 等待5秒再启动项目，确保程序以停止
+   sleep 5s
 fi
-echo 等待5秒再启动项目，确保程序以停止
-timeout /t 5
 echo 开始启动程序
 nohup java -jar /var/lib/jenkins/workspace/spring-boot_测试/demo-0.0.1-SNAPSHOT.jar>log.txt
