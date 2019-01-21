0ee3db6e46349d21de558d26a36b7cc22018e684
zhangle <778437470@qq.com>
2019-01-21 10:15:23 +0800

    添加执行启动脚本

--- startup.bat
+++ startup.bat
@@ -1,9 +1,9 @@
 echo 脚本杀死原进程
-taskkill /f /im A1javaw.exe
+pid=`ps -ef|grep demo-0.0.1-SNAPSHOT.jar | grep -v grep |awk '{print $2}'`
+if [ $pid ]; then
+    echo :App  is  running pid=$pid
+    kill -9 $pid
 echo 等待5秒再启动项目，确保程序以停止
 timeout /t 5
-del/f/s/q D:\jenkins\jenkins\workspace\Arunning\boot-0.0.1-SNAPSHOT.jar
-echo 原jar删除成功
-copy D:\jenkins\jenkins\workspace\bootCloud-base\target\boot-0.0.1-SNAPSHOT.jar D:\jenkins\jenkins\workspace\Arunning\boot-0.0.1-SNAPSHOT.jar
-echo 新jar复制成功
-start A1javaw -jar D:\jenkins\jenkins\workspace\Arunning\boot-0.0.1-SNAPSHOT.jar
+echo 开始启动程序
+nohup java -jar /var/lib/jenkins/workspace/spring-boot_测试/demo-0.0.1-SNAPSHOT.jar>log.txt
