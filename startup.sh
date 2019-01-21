a752158ed099a4418a491f50e6d341822fa68005
zhangle <778437470@qq.com>
2019-01-21 11:34:30 +0800

    修改为sh脚本

--- startup.sh
+++ startup.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 echo 脚本杀死原进程
 pid=`ps -ef|grep demo-0.0.1-SNAPSHOT.jar | grep -v grep |awk '{print $2}'`
-if [ $pid ]; then
-    echo :App  is  running pid=$pid
-    kill -9 $pid
+if [ ${pid} ]
+then
+   echo :App  is  running pid=${pid}
+else
+   kill -9 ${pid}
+fi
 echo 等待5秒再启动项目，确保程序以停止
 timeout /t 5
 echo 开始启动程序
