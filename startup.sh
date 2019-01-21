2fa0dec9952f942f87a8e53b4ac1c2bae92b1f58
zhangle <778437470@qq.com>
2019-01-21 12:22:50 +0800

    修改为sh脚本6

--- startup.sh
+++ startup.sh
@@ -3,10 +3,12 @@ echo 脚本杀死原进程
 pid=`ps -ef|grep demo-0.0.1-SNAPSHOT.jar | grep -v grep |awk '{print $2}'`
 if [ ${pid} ]
 then
-   echo :App  is  running pid=${pid}
+   echo App  is  running pid=${pid}
    kill -9 ${pid}
    echo 等待5秒再启动项目，确保程序以停止
    sleep 5s
 fi
 echo 开始启动程序
 nohup java -jar /var/lib/jenkins/workspace/spring-boot_测试/target/demo-0.0.1-SNAPSHOT.jar>log.txt 2>&1 &
+pid=`ps -ef|grep demo-0.0.1-SNAPSHOT.jar | grep -v grep |awk '{print $2}'`
+echo App  is  running pid=${pid}
