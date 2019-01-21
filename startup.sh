80cf2b7d5c8624ec91d5cf8cfb874d4e571a6809
zhangle <778437470@qq.com>
2019-01-21 14:44:35 +0800

    修改为sh脚本9

--- startup.sh
+++ startup.sh
@@ -11,4 +11,4 @@ fi
 echo 开始启动程序
 nohup java -jar /var/lib/jenkins/workspace/spring-boot_测试/target/demo-0.0.1-SNAPSHOT.jar>/var/lib/jenkins/workspace/spring-boot_测试/nohup.txt &
 pid2=`ps -ef|grep demo-0.0.1-SNAPSHOT.jar | grep -v grep |awk '{print $2}'`
-echo App  is  running pid=${pid2}
+echo App  is  running pid2=${pid2}
