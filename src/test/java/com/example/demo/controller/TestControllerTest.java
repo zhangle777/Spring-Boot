5bb965a53b600f6daee436fdf0b417f70cd928ca
zhangle <778437470@qq.com>
2019-03-05 10:10:15 +0800

    优化 单元测试

--- src/test/java/com/example/demo/controller/TestControllerTest.java
+++ src/test/java/com/example/demo/controller/TestControllerTest.java
@@ -1,6 +1,8 @@
 package com.example.demo.controller;
 
+import com.example.demo.constant.Constants;
 import com.example.demo.service.StudentService;
+import javax.servlet.http.Cookie;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -8,7 +10,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -16,7 +18,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-@RunWith(SpringRunner.class)
+@RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
 public class TestControllerTest {
 
@@ -25,22 +27,18 @@ public class TestControllerTest {
   @Autowired
   public StudentService studentService;
 
+  private Cookie cookie;
+
   private MockMvc mockMvc;
   @Before
   public void setUp() throws Exception {
     mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    cookie = new Cookie(Constants.LOGIN_COOKIE_SIGN,"");
+
   }
 
   @After
   public void tearDown() throws Exception {
-  }
-
-  @Test
-  public void hello() throws Exception{
-    mockMvc.perform(MockMvcRequestBuilders.get("/student/list").accept(MediaType.APPLICATION_JSON))
-        .andDo(MockMvcResultHandlers.print())
-        .andExpect(MockMvcResultMatchers.status().isOk())
-        .andReturn();
   }
 
   @Test
@@ -53,6 +51,7 @@ public class TestControllerTest {
 //        .param("age","22")
         .param("name","张")
         .param("orderBy","id")
+        .cookie(cookie)
 //        .param("sort","desc")
         .accept(MediaType.APPLICATION_JSON))
         .andDo(MockMvcResultHandlers.print())
